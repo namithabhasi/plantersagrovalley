81fb5a5a54a823b4f5c0d218f2189f0326aaaf7b
NejumaBenziya <sulfinejuma@gmail.com>
2026-07-30 20:27:19 +0530

    env error

--- SERVER/controllers/authController.js
+++ SERVER/controllers/authController.js
@@ -45,9 +45,8 @@ export const register = async (req, res) => {
     // Save JWT in cookie
    res.cookie("token", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite:
-    process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
@@ -109,9 +108,8 @@ if (!isMatch) {
     // Save cookie
     res.cookie("token", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite:
-    process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
@@ -139,10 +137,10 @@ if (!isMatch) {
 export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    });
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
     return res.status(200).json({
       success: true,
