2ff59508bfb6b7078fa4b384f6867ae3609d37d2
NejumaBenziya <sulfinejuma@gmail.com>
2026-07-30 20:44:27 +0530

    admin

--- SERVER/middleware/authMiddleware.js
+++ SERVER/middleware/authMiddleware.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 export const authenticate = async (req, res, next) => {
+   console.log("Cookies:", req.cookies);
+  console.log("Token:", req.cookies.token);
   try {
     const token = req.cookies.token;
 
